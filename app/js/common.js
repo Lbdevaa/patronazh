4ee0a44bb33d06431bdcb427dccf01e619a9b3c2
Merak <mesheryakov97@gmail.com>
2020-07-29 04:02:51 +0500

    main-page

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,36 @@
 $(function () {
   // Custom JS
-  $(".reviews__slider").slick({
+  $(".reviews-slider").slick({
     dots: true,
+    slidesToShow: 1,
+    nextArrow:
+      "<button type='button' class='reviews-slider__arrow reviews-slider__arrow--next'>Следующий </button>",
+    prevArrow:
+      "<button type='button' class='reviews-slider__arrow reviews-slider__arrow--prev'>Предыдущий</button>",
+  });
+  $(".services__items").slick({
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 9999,
+        settings: "unslick",
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          centerPadding: "40px",
+          // infinite: false,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  //mobile menu
+  $(".burger-menu").click(function () {
+    $(".mobile-menu").fadeToggle();
+    $(this).toggleClass("active");
   });
 });
